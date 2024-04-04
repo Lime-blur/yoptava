760db5ab3b8774d92ea3c30d8613b3e77962a84f
KristinaRub <kristina-rubtsova98@rambler.ru>
2024-04-04 23:59:56 +0300

    Fixes in StringUtils for Windows 10.

--- src/main/java/ru/limedev/yoptava/core/StringUtils.java
+++ src/main/java/ru/limedev/yoptava/core/StringUtils.java
@@ -5,7 +5,8 @@ public class StringUtils {
     public static final String EMPTY_STRING = "";
 
     public static String replaceBefore(String string, String before) {
-        return string.replaceFirst(".*?" + before, EMPTY_STRING);
+        String formattedBefore = before.replace("\\", "\\\\");
+        return string.replaceFirst(".*?" + formattedBefore, EMPTY_STRING);
     }
 
     public static String collectStringArray(String[] array) {
